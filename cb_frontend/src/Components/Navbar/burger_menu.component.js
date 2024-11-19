03ee00b643ec95f7b1c6c96e6f3831f6db087505
alextheprophecy <alexandrebourgoin23@gmail.com>
2024-11-19 22:09:45 +0100

    feat: logout button update

--- cb_frontend/src/Components/Navbar/burger_menu.component.js
+++ cb_frontend/src/Components/Navbar/burger_menu.component.js
@@ -79,7 +79,7 @@ const BurgerMenu = ({ t, isLoading, isEditing, setAskFeedback }) => {
                             {t('login.switch-account')}
                         </button>
                         <button 
-                            className="w-full text-red-600 bg-transparent hover:bg-red-800 px-4 py-2.5 rounded-lg text-sm md:text-base  font-medium transition-all duration-200 flex items-center justify-center border border-gray-700" 
+                            className="w-full text-red-600 bg-transparent hover:bg-red-800 px-4 py-2.5 rounded-lg text-sm md:text-base  font-medium transition-all duration-200 flex items-center justify-center" 
                             onClick={() => handleLogout()}
                         >
                             <LogOut className="w-5 h-5 mr-2" />
