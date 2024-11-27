72b57221391efef1713e18980939788fdaaae5ab
alextheprophecy <alexandrebourgoin23@gmail.com>
2024-11-27 23:33:55 +0100

    feat: test db for local use

--- cb_backend/config/config.js
+++ cb_backend/config/config.js
@@ -2,8 +2,14 @@ const mongoose = require("mongoose").default;
 const dotenv = require("dotenv");
 dotenv.config()
 
-const URI = `mongodb+srv://alexandrebourgoin23:${encodeURIComponent(process.env.MONGO_PASSWORD)}@cluster0.f2jrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`
-const CONNECT_DB = (uri= URI) => {
+// Define both URIs
+const PROD_URI = `mongodb+srv://alexandrebourgoin23:${encodeURIComponent(process.env.MONGO_PASSWORD)}@cluster0.f2jrp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const DEV_URI = 'mongodb://127.0.0.1:27017/cb_test_db';
+
+// Select URI based on environment
+const URI = process.env.NODE_ENV === 'production' ? PROD_URI : DEV_URI;
+
+const CONNECT_DB = (uri = URI) => {
     mongoose.Promise = global.Promise;
     mongoose.connect(uri).then(()=> {
         console.log('Database successfully connected!')
@@ -11,6 +17,5 @@ const CONNECT_DB = (uri= URI) => {
         console.log('Could not connect to database : ' + err)
     })
 };
-
 
 module.exports = CONNECT_DB;
